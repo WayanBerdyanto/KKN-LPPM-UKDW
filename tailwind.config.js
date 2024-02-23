1270aefaf6c8270721e04272e2d0453b8ebef2f5
Wayan Berdyanto <122479414+WayanBerdyanto@users.noreply.github.com>
2024-02-23 18:04:45 +0700

    LandingPage: "Navbar & HeroSection"

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#14b8a6",
+                dark: "#0f172a",
+                secondary: "#64748b",
+            },
+            backgroundImage: {
+                "hero-pattern": "url('/public/img/hero.jpg')",
+            },
+        },
     },
     plugins: [],
 };
