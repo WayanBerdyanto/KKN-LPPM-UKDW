1ad5729e273ce41322ea237c91c0bb1c70bd5882
WayanBerdyanto <aber.panak.atok@gmail.com>
2024-03-29 16:06:41 +0700

    Controll Dropdown Dosen with Alpine Js

--- resources/views/dosen/layouts/main.blade.php
+++ resources/views/dosen/layouts/main.blade.php
@@ -30,9 +30,7 @@
     </div>
     <script src="{{ asset('js/sidebar.js') }}"></script>
     <script src="{{ asset('js/dropdown.js') }}"></script>
-    <script src="{{ asset('js/ShowNotification.js') }}"></script>
-    <script src="{{ asset('js/ShowKegiatan.js') }}"></script>
-    <script src="{{ asset('js/ShowRencana.js') }}"></script>
+    <script defer src="{{ asset('js/alpine.min.js') }}"></script>
     @include('sweetalert::alert', ['cdn' => 'https://cdn.jsdelivr.net/npm/sweetalert2@9'])
     {{-- Larapex Chart --}}
     {{-- <script src="{{ $chart->cdn() }}"></script>
